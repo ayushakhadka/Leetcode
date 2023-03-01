1de39f6cf281bf73ebe77f4395de28d9175ec3b1
Ayusha Khadka <84257440+ayushakhadka@users.noreply.github.com>
2023-03-01 09:20:53 -0600

    Time: 46 ms (40.25%), Space: 52.3 MB (72.22%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,48 @@
+class Solution {
+    private void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+ 
+    private void heapify(int[] arr, int n, int i) {
+     
+        int largest = i; 
+        int left = 2 * i + 1;
+        int right = 2 * i + 2; 
+
+      
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            swap(arr, i, largest); 
+            heapify(arr, n, largest);
+        }
+    }
+
+    private void heapSort(int[] arr) {
+        int n = arr.length;
+        // Build heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Traverse elements one by one, to move current root to end, and
+        for (int i = n - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            // call max heapify 
+            heapify(arr, i, 0);
+        }
+    }
+
+    public int[] sortArray(int[] nums) {
+        heapSort(nums);
+        return nums;
+    }
+}
